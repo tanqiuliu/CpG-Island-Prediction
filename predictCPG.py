178ea35c542cf217ca835d31d4235a6df677cb91
tanqiul <liutanqiu@gmail.com>
2018-04-28 19:13:20 -0500

    viterbi added

--- predictCPG.py
+++ predictCPG.py
@@ -71,55 +71,86 @@ def getLogTransitionProb(freq):
 
 
 
-def viterbi(seq, log_trans_prob):
-    path = []
-    prob_mem = {j:{i:0 for i in STATE} for j in range(len(seq))}
-    prev_state_mem = {j:{i:0 for i in STATE} for j in range(len(seq))}
-    state_dict = {"A" : {"A+", "A-"}, "T" : {"T+", "T-"}, "G" : {"G+", "G-"}, "C" : {"C+", "C-"}}
-    for i in range(1,len(seq)):
-        print(i)
-        cur_state = state_dict[seq[i]]
-        for cur in cur_state:
-            max_prob = -1
-            max_prev = 0
-            for prev in STATE:
-                p = prob_mem[i - 1][prev] + log_trans_prob[prev][cur] + 0
-                if p > max_prob:
-                    max_prob = p
-                    max_prev = prev
+def viterbi(seq, log_trans_prob, log_prior_prob):
+path = []
+prob_mem = {j : { i : 0 for i in STATE} for j in range(len(seq))}
+prev_state_mem = {j : { i : None for i in STATE} for j in range(len(seq))}
+state_dict = {"A" : {"A+", "A-"}, "T" : {"T+", "T-"}, "G" : {"G+", "G-"}, "C" : {"C+", "C-"}}
+# prior
+for s in STATE:
+    prob_mem[0][s] = prior[s]
+#
+for i in range(1,len(seq)):
+    print(i)
+    cur_state = state_dict[seq[i]]
+    for cur in cur_state:
+        max_prob = -np.inf
+        max_prev = -1
+        for prev in STATE:
+            p = prob_mem[i - 1][prev] + log_trans_prob[prev][cur] + 0
+            if p > max_prob:
+                max_prob = p
+                max_prev = prev         
         prob_mem[i][cur] = max_prob
         prev_state_mem[i][cur] = max_prev
-    cur_prob = prob_mem[len(seq) - 1]
-    max_prob = max(cur_prob.values())
-    best_score = max_prob
-    for s in cur_prob.keys():
-        if cur_prob[s] == max_prob:
-            max_state = s
-    path.append(max_state)
-    #
-    for i in range(len(seq) - 1, 0, -1):
-        prev_max_state = prev_state_mem[i][max_state]
-        path.append(prev_max_state)
-        max_state = prev_max_state
-    #
-    result_path = []
-    for i in range(len(seq) - 1, -1, -1):
-        result_path.append(path[i])
-    return result_path, best_score
+    for s in STATE:
+        if s not in cur_state:
+            prob_mem[i][s] = -np.inf
+#
+cur_prob = prob_mem[len(seq) - 1]
+max_prob = max(cur_prob.values())
+best_score = max_prob
+for s in cur_prob.keys():
+    if cur_prob[s] == max_prob:
+        max_state = s
+path.append(max_state)
+#
+for i in range(len(seq) - 1, 0, -1):
+    prev_max_state = prev_state_mem[i][max_state]
+    path.append(prev_max_state)
+    max_state = prev_max_state
+#
+result_path = []
+for i in range(len(seq) - 1, -1, -1):
+    result_path.append(path[i])
+return result_path, best_score
 
 
 def iou(start1, end1, start2, end2):
-    intersect = min(end1, end2) - max(start1, start2)
-    union = max(end1, end2) - min(start1, start2)
-    return intersect / union
+    if start1 < end1 < start2 < end2:
+        return 0
+    elif start2 < end2 < start1 < end1: 
+        return 0
+    else:
+        intersect = min(end1, end2) - max(start1, start2)
+        union = max(end1, end2) - min(start1, start2)
+        return intersect / union
 
 
-def score(cpg_gt, cpg_pred):
+def score(cpg_gt, cpg_pred, thresholds=[0.5]):
     """
     cpg_gt = train_cpg.iloc[0:5]
     aa = np.array([[29000,29500],[134800,135300],[200000,200400],[350000,360000],[380000,383000]])
     cpg_pred = pd.DataFrame(aa,columns=['chromStart','chromEnd'])
     """
+    iou_matrix = np.zeros((len(cpg_gt), len(cpg_pred)))
+    for i in range(len(cpg_gt)):
+        for j in range(len(cpg_pred)):
+            iou_matrix[i,j] = iou(cpg_gt.iloc[i]['chromStart'], cpg_gt.iloc[i]['chromEnd'], cpg_pred.iloc[j]['chromStart'], cpg_pred.iloc[j]['chromEnd'])
+    scores = []
+    for threshold in thresholds:
+        match = (iou_matrix > threshold) * 1
+        TP = np.sum(np.sum(match))
+        FP = len(cpg_pred) - TP
+        FN = len(cpg_gt) - TP
+        scores.append(TP / (TP + FP + FN))
+    return np.sum(scores) / len(thresholds)
+
+
+# TO DO:
+# post processing
+# further improvement
+
     
 
 
